3c8e3a851a0be19deaf50d271d56bd816da936c2
Hamza Gharbi <hamza-2.gharbi@polymtl.ca>
2025-10-17 22:25:32 -0400

    Remise du TP2 + Finalisation de l'exercice bonus

--- Exercice_bonus.py
+++ Exercice_bonus.py
@@ -79,20 +79,25 @@ def deplacer_serveur(grille, serveur_pos, direction):
     nouvelle_pos = serveur_pos
     
     # TODO: Calculer la nouvelle position selon la direction
-    grille_max_x, grille_max_y = len(grille[0]) - 1, len(grille) - 1
+    grille_max_y, grille_max_x = len(grille) - 1, len(grille[0]) - 1
+
+    restreint_haut = [(1, 2), (2, 1), (2, 3), (4, 1), (4, 3)]
+    restreint_bas = [(0, 1), (0, 3), (2, 1), (2, 3)]
+    restreint_gauche = [(0, 3), (1, 4), (3, 4), (1, 2), (3, 2)]
+    restreint_droit = [(0, 1), (1, 0), (3, 0), (1, 2), (3, 2)]
 
     match direction:
         case "w":
-            if nouvelle_pos[0] > 0:
+            if nouvelle_pos[0] > 0 and nouvelle_pos not in restreint_haut:
                 nouvelle_pos = (nouvelle_pos[0] - 1, nouvelle_pos[1])
         case "s":
-            if nouvelle_pos[0] < grille_max_y:
+            if nouvelle_pos[0] < grille_max_y and nouvelle_pos not in restreint_bas:
                 nouvelle_pos = (nouvelle_pos[0] + 1, nouvelle_pos[1])
         case "a":
-            if nouvelle_pos[1] > 0:
+            if nouvelle_pos[1] > 0 and nouvelle_pos not in restreint_gauche:
                 nouvelle_pos = (nouvelle_pos[0], nouvelle_pos[1] - 1)
         case "d":
-            if nouvelle_pos[1] < grille_max_x:
+            if nouvelle_pos[1] < grille_max_x and nouvelle_pos not in restreint_droit:
                 nouvelle_pos = (nouvelle_pos[0], nouvelle_pos[1] + 1)
 
     return nouvelle_pos
@@ -117,9 +122,20 @@ def prendre_commande(grille, serveur_pos, commandes_en_attente):
     
     # TODO: Vérifier si une table avec client '!' est adjacente
     # Si oui: changer '!' en 'T', ajouter position à commandes_en_attente
-    # Gagner 10 points
-
+    serveur_y, serveur_x = serveur_pos[0], serveur_pos[1]
+    pos_adjacent = [(serveur_y + 1, serveur_x),
+                    (serveur_y - 1, serveur_x),
+                    (serveur_y, serveur_x + 1),
+                    (serveur_y, serveur_x - 1)]
     
+    for y, rangee in enumerate(nouvelle_grille):
+        for x, pos in enumerate(rangee):
+            if pos == "!" and (y, x) in pos_adjacent:
+                nouvelle_grille[y][x] = "T"
+                nouvelles_commandes.append((y, x))
+                succes = True
+                points += 10
+
     return succes, nouvelle_grille, nouvelles_commandes, points
 
 
@@ -140,7 +156,15 @@ def livrer_commande(grille, serveur_pos, serveur_porte_commande, commandes_prete
     points = 0
     
     # TODO: Si serveur_porte_commande et serveur à côté d'une table dans commandes_pretes
-    # Livrer la commande et gagner 20 points
+    for table in commandes_pretes:
+        pos_adjacent = [(table[0] + 1, table[1]),
+                        (table[0] - 1, table[1]),
+                        (table[0], table[1] + 1),
+                        (table[0], table[1] - 1)]
+        if serveur_porte_commande and serveur_pos in pos_adjacent:
+            succes = True
+            points += 20
+            commandes_pretes.remove(table)
     
     return succes, points
 
@@ -163,9 +187,9 @@ def generer_nouveaux_clients(grille, tables_positions, probabilite=0.3):
     for position in tables_positions:
         if nouvelle_grille[position[0]][position[1]] == "T" and random.random() <= probabilite:
             nouvelle_grille[position[0]][position[1]] = "!"
+            break # Décommenter va limiter l'apparition à un seul client par cycle (mode difficile ?)
     
     return nouvelle_grille
-
 
 def jouer():
     """
@@ -187,7 +211,7 @@ def jouer():
     print("Appuyez sur Entrée pour commencer...")
     input()
     
-    # TODO: Implémenter la boucle de jeu
+    # TODO: Implémenter la boucle de jeu  
     while tours < max_tours:
         # Affichage de l'état
         afficher_restaurant(grille, serveur_pos, score, commandes_en_attente)
@@ -199,32 +223,49 @@ def jouer():
             print("Touche invalide, veuillez réessayer")
             touche = input("-> ").lower()
 
-        # Vérifie et effectue la bonne touche
+        # Vérifie et traite la bonne touche / action
         match touche:
             case ("w" | "a" | "s" | "d"):
                 serveur_pos = deplacer_serveur(grille, serveur_pos, touche)
 
             case "p":
-                prendre_commande(grille, serveur_pos, commandes_en_attente)
-            
+                ping, grille, commandes_en_attente, score_prise = prendre_commande(grille, serveur_pos, commandes_en_attente)
+                score += score_prise
+                score_prise = 0
+                if ping:
+                    print("Commande prise !")
+
             case "l":
-                livrer_commande(grille, serveur_pos, serveur_porte_commande, commandes_pretes)
+                ping, score_livraison = livrer_commande(grille, serveur_pos, serveur_porte_commande, commandes_pretes)
+                score += score_livraison
+                score_livraison = 0
+                if ping:
+                    print("Commande livré !")
+                    if commandes_pretes == []:
+                        serveur_porte_commande = False
 
         # Génère un nouveau client aux 3 tours
-        if tours % 3 == 0:
-            generer_nouveaux_clients(grille, tables_pos)
+        if (tours + 1) % 3 == 0:
+            grille = generer_nouveaux_clients(grille, tables_pos)
+            for y, rangee in enumerate(grille):
+                for x, pos in enumerate(rangee):
+                    if pos == "!" and (y, x) not in commandes_en_attente:
+                        commandes_en_attente.append((y, x))
 
+        # Gère le cycle de la cuisine
+        pos_cuisine_adjacent = [(pos_cuisine[0] + 1, pos_cuisine[1]), 
+                                (pos_cuisine[0], pos_cuisine[1] - 1), 
+                                (pos_cuisine[0], pos_cuisine[1] + 1)]
         
-
+        if serveur_pos in pos_cuisine_adjacent and commandes_en_attente:
+            print("Commande préparé !")
+            serveur_porte_commande = True
+            commandes_pretes.extend(commandes_en_attente)
+            commandes_en_attente = []
+            
+        # Incrémente les tours
         tours += 1
 
-    # while tours < max_tours:
-    #     1. Afficher l'état
-    #     2. Lire l'entrée utilisateur
-    #     3. Traiter l'action (déplacement, prendre, livrer)
-    #     4. Générer nouveaux clients (tous les 3 tours)
-    #     5. Mettre à jour le score
-    #     6. Incrémenter tours
     
     print(f"\n=== PARTIE TERMINÉE ===")
     print(f"Score final: {score}")
